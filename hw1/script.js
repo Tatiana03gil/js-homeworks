0ed84ea287ccc3741f5084e6c176341a33b69b6b
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2024-08-01 01:44:22 +0300

    hw-1

---
+++ hw1/script.js
@@ -0,0 +1,13 @@
+// task 1
+
+let celsiusDegrees = 0;
+let fahrenheitDegrees = (9 / 5) * celsiusDegrees + 32;
+console.log (`Celsius Degrees = ${celsiusDegrees}`);
+console.log (`Fahrenheit Degrees = ${fahrenheitDegrees}`);
+
+//task 2
+
+let name = 'Tatiana';
+let admin = name;
+
+console.log(admin);
