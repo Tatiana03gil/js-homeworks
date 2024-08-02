2243aac96a3cb845eca3e0e2558760a33eafacf9
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2024-08-03 01:47:42 +0300

    homework 3

---
+++ hw3/script.js
@@ -0,0 +1,63 @@
+// Задание 1
+
+/*const cube = (num) => {
+  return num ** 3;
+};
+
+console.log(cube(2) + cube(3));
+*/
+
+//Задание 2
+
+/*const salary = (num) => {
+  console.log(`Размер заработной платы за вычетом налогов равен ${num * 0.87}`);
+};
+
+let num = Number(prompt("Введите число: "));
+if (isNaN(num)) {
+  console.log("значение задано неверно");
+} else {
+  salary(num);
+}*/
+
+//Задание 3
+
+/*const maxNumber = (num1, num2, num3) => {
+  if (num1 > num2) {
+    if (num1 > num3) {
+      console.log(`Максимальное значение ${num1}`);
+    } else `Максимальное значение ${num3}`;
+  } else {
+    if (num2 > num3) {
+      console.log(`Максимальное значение ${num2}`);
+    } else {
+      console.log(`Максимальное значение ${num3}`);
+    }
+  }
+};
+
+let num1 = Number(prompt("Введите первое число:"));
+let num2 = Number(prompt("Введите второе число:"));
+let num3 = Number(prompt("Введите третье число:"));
+
+maxNumber(num1, num2, num3);*/
+
+//Задание 4
+
+/*const sum = (num1, num2) => {
+    return num1+num2;
+}
+
+const minus = (num1, num2) => {
+    return num1-num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1*num2;
+}
+
+const divide = (num1, num2) => {
+    return num1/num2;
+}
+
+console.log(sum(2, 6));*/
